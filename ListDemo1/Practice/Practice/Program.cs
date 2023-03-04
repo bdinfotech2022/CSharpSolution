6077c06e4dee8166def7a3278f46d61d288dfbeb
bdinfotech2022 <bdinfotech2022@gmail.com>
2023-03-04 10:36:55 -0500

    practice

--- ListDemo1/Practice/Practice/Program.cs
+++ ListDemo1/Practice/Practice/Program.cs
@@ -6,11 +6,48 @@ using System.Threading.Tasks;
 
 namespace Practice
 {
-    internal class Program
+    public class Program
     {
+
+        // access modifiers: public static
+        // a method name: GetLen(){ } 
+        // input parameter: string
+        // output / return type: length (how many characters) - int
+
+        public static int GetLen(string text) {
+
+            // logic / algorithm
+
+            int len = text.Length;
+
+
+            return len;
+
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // calling the method inside the Main():  actual name of our method + actual value
+            Console.WriteLine(GetLen("atikur rahman"));
+
+
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
